4497a782cdb73a8565a68ab4d8c99ea731516fdb
Kauã Kouqui <kaua.kouqui@gmail.com>
2025-10-04 01:50:17 -0300

    feat: Adding spring boot

---
+++ src/main/java/com/fut/fut360/Fut360Application.java
@@ -0,0 +1,13 @@
+package com.fut.fut360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fut360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fut360Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fut/fut360/Fut360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fut.fut360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fut360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
