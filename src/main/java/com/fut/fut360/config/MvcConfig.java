55ffecc02df837c8b77b1ad1ccd32393b6b78e0d
Kauã Kouqui <kaua.kouqui@gmail.com>
2025-12-04 21:32:40 -0300

    fix(altetas): Ajustando salvamente de imagens dos atletas

--- src/main/java/com/fut/fut360/config/MvcConfig.java
+++ src/main/java/com/fut/fut360/config/MvcConfig.java
@@ -3,6 +3,7 @@ package com.fut.fut360.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 import java.nio.file.Paths;
 
 @Configuration
@@ -10,12 +11,11 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // Pega o caminho absoluto da pasta "uploads"
-        // (Ex: "file:///C:/Workspace/projeto_integrador4/uploads/")
-        String uploadDir = Paths.get(System.getProperty("user.dir"), "uploads").toUri().toString();
+        // Define a pasta "uploads" na raiz do projeto
+        String uploadPath = Paths.get("uploads").toAbsolutePath().toUri().toString();
 
-        // Mapeia a URL "/uploads/**" para a pasta física "uploads/"
+        // Mapeia a URL /uploads/** para a pasta física
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations(uploadDir);
+                .addResourceLocations(uploadPath);
     }
 }
